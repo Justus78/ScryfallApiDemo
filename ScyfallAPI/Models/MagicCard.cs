bcb3c235b53cfb2346bea128b10a74f35ed5f25f
Justus78 <stars787878@aol.com>
2024-10-04 19:59:44 -0400

    inital commit for project

---
+++ ScyfallAPI/Models/MagicCard.cs
@@ -0,0 +1,11 @@
+﻿namespace ScyfallAPI.Models
+{
+    public class MagicCard
+    {
+        public string Name { get; set; }
+        public string ManaCost { get; set; }
+        public string TypeLine { get; set; }
+        public string OracleText { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
